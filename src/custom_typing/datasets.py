5e93c08b29370b5308ea4383ae4fd955dc35438a
gunjanpatil <gunjangpatil23@gmail.com>
2021-01-13 18:44:53 -0800

    removed unwanted class

--- src/custom_typing/datasets.py
+++ src/custom_typing/datasets.py
@@ -11,14 +11,3 @@ class SimplifiedNaturalQADataset(Dataset):
 
     def __getitem__(self, index):
         return self.id_list[index]
-
-class QADatasetWithNegativeExamples(SimplifiedNaturalQADataset):
-    def __init__(self, id_list, neg_id_list) -> object:
-        super().__init__(id_list)
-        self.neg_id_list = neg_id_list
-
-    def __len__(self):
-        return len(self.id_list)
-
-    def __getitem__(self, index):
-        return self.id_list[index]
