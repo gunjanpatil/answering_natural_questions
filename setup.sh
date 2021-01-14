900202e3b807005d913340fc1bd3d3f6b88541e3
gunjanpatil <gunjangpatil23@gmail.com>
2021-01-13 18:57:55 -0800

    adding bash script to download datasets, setup and install packages

---
+++ setup.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/NVIDIA/apex
+cd apex
+pip install -v --disable-pip-version-check --no-cache-dir ./
+pip install -r requirements.txt
+cd ../
+sudo pip install gsutil
+mkdir datasets
+cd datasets
+mkdir natural_questions_simplified
+cd natural_questions_simplified
+gsutil -m cp -R gs://natural_questions/v1.0-simp* .
+cd v1.0-simplified/
+gzip -d simplified-nq-train.jsonl.gz
+gzip -d nq-dev-all.jsonl.gz
